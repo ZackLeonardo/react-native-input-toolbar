002a784facb908601ced19c777261c11d6eb4bb5
ZackLeonardo <maiz9088@icloud.com>
2017-09-29 10:16:08 +0800

    add icon test

--- example/test.js
+++ example/test.js
@@ -9,18 +9,33 @@ import {
 import PropTypes from 'prop-types';
 
 import InputToolbar from '@zdy/react-native-input-toolbar';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Test extends React.Component{
   constructor(props){
     super(props);
 
+    this.onPressActionButton = this.onPressActionButton.bind(this);
+    this.renderIcon = this.renderIcon.bind(this);
+
+  }
+
+  renderIcon(){
+    return (<Icon name={'edit'} size={24} color={'#b2b2b2'}/>);
+  }
+
+  onPressActionButton(){
+    console.log('onPressActionButton');
   }
 
   render(){
 
     return (
       <View style={styles.container}>
-        <InputToolbar/>
+        <InputToolbar
+          icon={this.renderIcon}
+          onPressActionButton={this.onPressActionButton}
+          />
       </View>
 
     );
